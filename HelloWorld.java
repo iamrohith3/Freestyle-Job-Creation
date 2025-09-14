2f5cb610304b3a26aafb671fd0fbae66ee7a28a2
iamrohith3 <56444586+iamrohith3@users.noreply.github.com>
2025-09-14 11:31:11 +0530

    Update HelloWorld.java
    indentation changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-public static void main(Strings[] args) {
-System.out.println("Hello World");
-}
+  public static void main(Strings[] args) {
+    System.out.println("Hello World");
+  }
 }
